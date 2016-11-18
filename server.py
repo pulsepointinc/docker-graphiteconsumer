f7f0f597b5752682867e0834380f405f4465bd04
Andrew R Kursar <akursar@gmail.com>
2016-11-18 16:46:51 -0500

    Move to alpine image, log exceptions

--- server.py
+++ server.py
@@ -1,25 +1,44 @@
-from kafka import KafkaConsumer
-from os import environ
+import logging
 import socket
+from os import environ
 
-grapserver = environ["grapserver"] if "grapserver" in environ else "0.0.0.0"
-grapport = environ["grapport"] if "grapport" in environ else 2003
-topic = environ["topic"] if "topic" in environ else "mytopic"
-bootstrap_servers = environ["bootstrap_servers"] if "bootstrap_servers" in environ else "localhost:9092"
+from kafka import KafkaConsumer
 
-consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_servers, group_id=None, max_partition_fetch_bytes=10485760, auto_offset_reset="earliest")
+
+def get_env_config(var, default):
+    return environ[var] if var in environ else default
+
+grapserver = get_env_config("grapserver", "0.0.0.0")
+grapport = int(get_env_config("grapport", 2003))
+topic = get_env_config("topic", "mytopic")
+bootstrap_servers = get_env_config("bootstrap_servers", "localhost:9092")
+group_id = get_env_config("group_id", None)
+max_partition_fetch_bytes = int(get_env_config("max_partition_fetch_bytes", 10485760))
+auto_offset_reset = get_env_config("auto_offset_reset", "earliest")
+loglevel = get_env_config("loglevel", "INFO")
+
+conf = {
+    "bootstrap_servers": bootstrap_servers,
+    "group_id": group_id,
+    "max_partition_fetch_bytes": max_partition_fetch_bytes,
+    "auto_offset_reset": auto_offset_reset,
+}
+
+level = getattr(logging, loglevel.upper())
+logging.basicConfig(level=level)
+logging.info("Creating KafkaConsumer for topic(s) {} with config {}".format(topic, str(conf)))
+consumer = KafkaConsumer(topic, **conf)
 
 for msg in consumer:
-	try:
-		key, val, ts = msg.value.split(' ') 
-	except TypeError or ValueError:
-    		print ("Value Failure")
-	else:
-		if ts.isdigit() and val.isdigit():
-			print (key, val, ts)
-			message = ('%s %s %s\n' % (key, val, ts))
-			print ('sending message:\n%s' % message)
-			sock = socket.socket()
-			sock.connect((grapserver, grapport))
-			sock.sendall(message)
-			sock.close()
+    try:
+        key, val, ts = msg.value.split(' ') 
+    except:
+        logging.exception("Failed in extracting metric from {}".format(str(msg)))
+    else:
+        if ts.isdigit() and val.isdigit():
+            message = ("{} {} {}\n".format(key, val, ts))
+            logging.debug("Sending message: {}\n".format(message))
+            sock = socket.socket()
+            sock.connect((grapserver, grapport))
+            sock.sendall(message)
+            sock.close()
